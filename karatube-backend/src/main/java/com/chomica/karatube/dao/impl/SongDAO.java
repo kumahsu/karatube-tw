6a6a8f2df665a2a03cde2946d5d1ac98650a1fed
kuma.hsu <kuma0928002898@gmail.com>
2016-10-05 21:25:17 +0800

    #9 Implement Song CRUD

---
+++ karatube-backend/src/main/java/com/chomica/karatube/dao/impl/SongDAO.java
@@ -0,0 +1,58 @@
+package com.chomica.karatube.dao.impl;
+
+import javax.persistence.TypedQuery;
+
+import com.chomica.karatube.constant.SongCategory;
+import com.chomica.karatube.dao.ISongDAO;
+import com.chomica.karatube.model.QueryListData;
+import com.chomica.karatube.model.entity.SongEntity;
+import com.chomica.karatube.util.DaoUtil;
+import com.chomica.karatube.util.HQL;
+
+public class SongDAO extends BaseJpaDAO<SongEntity> implements ISongDAO {
+   // ---------------------------------------------------------------
+   public SongDAO() {
+      super(SongEntity.class);
+   }
+
+   // ---------------------------------------------------------------
+   @Override
+   public QueryListData<SongEntity> findSongsBySinger(Integer start, 
+                                                        Integer size, 
+                                                        String singerId) {
+      HQL.Builder<SongEntity> builder = new HQL.Builder<SongEntity>();
+      builder.select(super.clazz)
+             .where(HQL.equals("singer.singer_id", singerId));
+      TypedQuery<SongEntity> query = builder.toHql(super.em);
+      return DaoUtil.getPagedResult(query, start, size);
+   }
+
+   @Override
+   public QueryListData<SongEntity> findSongsByName(Integer start, 
+                                                      Integer size, 
+                                                      String name) {
+      String condition = "%" + name + "%";
+      HQL.Builder<SongEntity> builder = new HQL.Builder<SongEntity>();
+      builder.select(super.clazz)
+             .where(HQL.like("song_name", condition));
+      TypedQuery<SongEntity> query = builder.toHql(super.em);
+      return DaoUtil.getPagedResult(query, start, size);
+   }
+   
+   @Override
+   public QueryListData<SongEntity> findSongsByCondition(Integer start, 
+                                                           Integer size, 
+                                                           SongCategory category,
+                                                           String name)
+   {
+      HQL.Builder<SongEntity> builder = new HQL.Builder<SongEntity>();
+      builder.select(super.clazz)
+             .where(HQL.equals("song_category", category.code()));
+      if(name != null) {
+         String condition = "%" + name + "%";
+         builder.and(HQL.like("song_name", condition));
+      }
+      TypedQuery<SongEntity> query = builder.toHql(super.em);
+      return DaoUtil.getPagedResult(query, start, size);
+   }
+}
